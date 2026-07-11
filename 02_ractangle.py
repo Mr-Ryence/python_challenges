8fea7e98419e202b98ffcde7b2124a6ad5371f73
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 22:43:25 +0530

    challenge 02 updated

--- 02_ractangle.py
+++ 02_ractangle.py
@@ -1,6 +1,10 @@
 l=int(input("Enter the length of rectangle: "))
 b=int(input("Enter the breath of rectangle: "))
-perimeter=2*(l+b)
-area=l*b
-print(f"The perimeter of the rectangle is {perimeter}")
-print(f"The area of the rectangle is {area}")
+if (l>0 and b>0): 
+     perimeter=2*(l+b)
+     area=l*b
+     print(f"The perimeter of the rectangle is {perimeter}")
+     print(f"The area of the rectangle is {area}")
+else: 
+     print("Please enter only positive value.")
+     
