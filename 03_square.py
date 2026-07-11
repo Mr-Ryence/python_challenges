0a8ecd9777a9e18bc49aebf493327bf92ca18e25
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 22:45:34 +0530

    code 03 updated

--- 03_square.py
+++ 03_square.py
@@ -1,5 +1,9 @@
 s=int(input("Enter the side of the square = "))
-perimeter=4*s
-area=s*s
-print(f"The perimeter of the square is {perimeter}")
-print(f"The area of the square is {area}")
+if(s>0): 
+     perimeter=4*s
+     area=s*s
+     print(f"The perimeter of the square is {perimeter}")
+     print(f"The area of the square is {area}")
+else: 
+     print("Please enter only positive value.")
+     
