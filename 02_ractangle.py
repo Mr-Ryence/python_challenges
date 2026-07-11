6c472b24d60027328e4b7d8c3c39e4b89714c7eb
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 21:09:41 +0530

    created challenge 02

---
+++ 02_ractangle.py
@@ -0,0 +1,6 @@
+l=int(input("Enter the length of rectangle: "))
+b=int(input("Enter the breath of rectangle: "))
+perimeter=2*(l+b)
+area=l*b
+print(f"The perimeter of the rectangle is {perimeter}")
+print(f"The area of the rectangle is {area}")
