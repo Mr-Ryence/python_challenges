d494895b7cba7a1ac2fd70070c43c1db58903532
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 23:07:29 +0530

    code 06 updated

--- 06_circle.py
+++ 06_circle.py
@@ -1,5 +1,8 @@
 r=int(input("Enter the radius of the circle = "))
-perimeter=2*(22/7)*r
-area=(22/7)*r*r
-print(f"The perimeter of the circle is {perimeter}")
-print(f"The area of the circle is {area}")
+if(r>0):
+     perimeter=2*(22/7)*r
+     area=(22/7)*r*r
+     print(f"The perimeter of the circle is {perimeter}")
+     print(f"The area of the circle is {area}")
+else: 
+     print("please enter only positive numer")
