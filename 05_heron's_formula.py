1d1514de380c257851a109db71e8df41c3c13a85
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 23:03:31 +0530

    code 05 updated

--- 05_heron's_formula.py
+++ 05_heron's_formula.py
@@ -2,6 +2,12 @@ import math
 a=int(input("Enter the first side = "))
 b=int(input("Enter the second side = "))
 c=int(input("Enter the third side = "))
-s=(a+b+c)/2
-area=math.sqrt((s*(s-a)*(s-b)*(s-c)))
-print(f"The area of the trangle is {area}")
+if(a>0 and b>0 and c>0):
+     if(a+b>c and a+c>b and b+c>a): 
+          s=(a+b+c)/2
+          area=math.sqrt((s*(s-a)*(s-b)*(s-c)))
+          print(f"The area of the trangle is {area}")
+     else: 
+          print("This is not follow the pythagorus theorem.")
+else: 
+     print("please enter only positive number.");
