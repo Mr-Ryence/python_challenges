17f0f23d4be006aa19e8c412a2cf2e1d01629f61
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 21:31:26 +0530

    created challenge 05

---
+++ 05_heron's_formula.py
@@ -0,0 +1,7 @@
+import math
+a=int(input("Enter the first side = "))
+b=int(input("Enter the second side = "))
+c=int(input("Enter the third side = "))
+s=(a+b+c)/2
+area=math.sqrt((s*(s-a)*(s-b)*(s-c)))
+print(f"The area of the trangle is {area}")
