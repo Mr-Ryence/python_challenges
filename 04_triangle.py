6e3b94c6dc46993e2980b3bc92c75f50d1fa8ff7
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 22:51:41 +0530

    code 04 updated

--- 04_triangle.py
+++ 04_triangle.py
@@ -1,4 +1,7 @@
 b=int(input("Enter the base of the triangle = "))
 h=int(input("Enter the height of the triangle = "))
-area=1/2*(b*h)
-print(f"The area of the trianlge is {area}")
+if(b>0 and h>0): 
+     area=1/2*(b*h)
+     print(f"The area of the trianlge is {area}")
+else: 
+     print("please enter only positive number.")
