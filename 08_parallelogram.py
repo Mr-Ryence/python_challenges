c5286f3bc45b859fc5e3d0572ea7aeaa499afeb2
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 22:28:27 +0530

    created 08 challenge

---
+++ 08_parallelogram.py
@@ -0,0 +1,6 @@
+b=int(input("Enter the base of the parallelogram = "))
+h=int(input("Enter the height of the parallelogram = "))
+perimeter=2*(b+h)
+area=b*h
+print(f"The perimeter of the parallelogram {perimeter}")
+print(f"The area of the parallelogram {area}")
