b482ae5e3236e8e1a9fd2dfd5fb9d12743073bd9
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 21:37:27 +0530

    created challege 06

---
+++ 06_circle.py
@@ -0,0 +1,5 @@
+r=int(input("Enter the radius of the circle = "))
+perimeter=2*(22/7)*r
+area=(22/7)*r*r
+print(f"The perimeter of the circle is {perimeter}")
+print(f"The area of the circle is {area}")
