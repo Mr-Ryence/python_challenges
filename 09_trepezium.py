83780d37e3923d079cd6ae6636976e6fdfd0561e
Ryence <thanedarkumhar55@gmail.com>
2026-07-11 22:36:11 +0530

    created challenge  09

---
+++ 09_trepezium.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first parallel side = "))
+b=int(input("Enter the second parallel side = "))
+h=int(input("Enter the height of the trepezium = "))
+area=((a+b)*h)/2 
+print(f"The area of the trepezium is {area}")
