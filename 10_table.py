faa3edb78e1317d70d80210e619830666e685e59
Ryence <thanedarkumhar55@gmail.com>
2026-07-13 22:33:55 +0530

    created challenge 11

--- 10_table.py
+++ 10_table.py
@@ -1,3 +1,8 @@
 num = int(input("Enter any number = "))
 for i in range(1, 11, 1):
     print(f"{num}*{i}={num*i}")
+
+# This is second method
+# num = int(input("Enter "))
+# for i in range(num, num * 10 + 1, num):
+#     print(i)
