114e3b959ebdef62f7012c64c23e754bd11a3c7a
Ryence <ryence06@gmail.com>
2026-07-16 16:08:13 +0530

    created challenge 29

---
+++ 29_square_num.py
@@ -0,0 +1,6 @@
+import math
+num=int(input("Enter the number that you want to find the square = "))
+if(num<0):
+     print(f"Please enter only positive number.")
+else: 
+     print(f"The square of {num} is = {num*num}")
