4f85b7969d474ba38af1972a35d00d75baec0df9
Ryence <ryence06@gmail.com>
2026-07-20 20:42:04 +0530

    created challenge 32

---
+++ 32_square_root.py
@@ -0,0 +1,8 @@
+num=int(input("Enter any number = "))
+# firth method
+if(num<=0):
+     print("Invalid value.(Enter only positive value.)")
+else: 
+     result =num**0.5
+     print(f"The square root of {num} = {result}")
+     
